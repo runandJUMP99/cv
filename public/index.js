19db6d79bf929ad8610b32b694cdcd42f496b1c1
Steven Velez <stevendvelez@yahoo.com>
2020-07-09 20:09:04 -0400

    update images; update 404 page; update text; add responsive elements

--- public/index.js
+++ public/index.js
@@ -1,13 +1,28 @@
-const mainNavFixed = document.querySelector(".main-nav__fixed")
-let visible = false
+const backdrop = document.querySelector(".backdrop");
+const mainNavFixed = document.querySelector(".main-nav__fixed");
+const sidedrawer = document.querySelector(".sidedrawer");
+const toggleButton = document.querySelectorAll(".toggle-button__container");
+let visible = false;
 
 window.addEventListener("scroll", function() {
     const currentScrollPos = window.pageYOffset;
     let visible = currentScrollPos > 475;
-    console.log(currentScrollPos);
+
     if (visible) {
         mainNavFixed.classList.add("main-nav__fixed__show")
     } else {
         mainNavFixed.classList.remove("main-nav__fixed__show")
     }
+});
+
+toggleButton.forEach(function(button) {
+    button.addEventListener("click", function() {
+        backdrop.classList.add("open");
+        sidedrawer.classList.add("open");
+    });
+});
+
+backdrop.addEventListener("click", function() {
+    backdrop.classList.remove("open");
+    sidedrawer.classList.remove("open");
 });
